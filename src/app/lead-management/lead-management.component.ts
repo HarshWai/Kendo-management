9a52918198d0e4732a6e84360c8ec409d186f5a2
HarshWai <harshvardhan.gunjal@waiin.com>
2025-04-16 12:11:32 +0530

    feat(lead-management): enhance lead management grid with new features and styling
    - Updated lead-management.component.html to implement Kendo Grid with enhanced features including multi-select, export options, and improved layout.
    - Refactored lead-management.component.css to streamline button styles and add new classes for customer photo and name display.
    - Enhanced lead-management.component.ts to include filtering functionality and dynamic data binding for grid.
    - Added localization support in main.ts and updated tsconfig files to include Angular localization types.

--- src/app/lead-management/lead-management.component.ts
+++ src/app/lead-management/lead-management.component.ts
@@ -1,11 +1,88 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { KENDO_CHARTS } from "@progress/kendo-angular-charts";
+import {
+  DataBindingDirective,
+  KENDO_GRID,
+  KENDO_GRID_EXCEL_EXPORT,
+  KENDO_GRID_PDF_EXPORT,
+} from "@progress/kendo-angular-grid";
+import { KENDO_INPUTS } from '@progress/kendo-angular-inputs';
+import { process } from "@progress/kendo-data-query";
+import { SVGIcon, fileExcelIcon, filePdfIcon } from "@progress/kendo-svg-icons";
+import { employees } from "./employees";
+import { images } from "./images"
 
 @Component({
   selector: 'app-lead-management',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, KENDO_GRID, KENDO_CHARTS, KENDO_INPUTS, KENDO_GRID_EXCEL_EXPORT, KENDO_GRID_PDF_EXPORT],
   templateUrl: './lead-management.component.html',
   styleUrl: './lead-management.component.css'
 })
-export class LeadManagementComponent {
+export class LeadManagementComponent implements OnInit {
+  @ViewChild(DataBindingDirective) dataBinding!: DataBindingDirective;
+  public gridData: unknown[] = employees;
+  public gridView!: unknown[];
 
+  public mySelection: string[] = [];
+  public pdfSVG: SVGIcon = filePdfIcon;
+  public excelSVG: SVGIcon = fileExcelIcon;
+
+  public ngOnInit(): void {
+    this.gridView = this.gridData;
+  }
+
+  public onFilter(value: Event): void {
+    const inputValue = value;
+
+    this.gridView = process(this.gridData, {
+      filter: {
+        logic: "or",
+        filters: [
+          {
+            field: "full_name",
+            operator: "contains",
+            value: inputValue,
+          },
+          {
+            field: "job_title",
+            operator: "contains",
+            value: inputValue,
+          },
+          {
+            field: "budget",
+            operator: "contains",
+            value: inputValue,
+          },
+          {
+            field: "phone",
+            operator: "contains",
+            value: inputValue,
+          },
+          {
+            field: "address",
+            operator: "contains",
+            value: inputValue,
+          },
+        ],
+      },
+    }).data;
+
+    this.dataBinding.skip = 0;
+  }
+
+  public photoURL(dataItem: { img_id: string; gender: string }): string {
+    const code: string = dataItem.img_id + dataItem.gender;
+    const image: { [Key: string]: string } = images;
+
+    return image[code];
+  }
+
+  public flagURL(dataItem: { country: string }): string {
+    const code: string = dataItem.country;
+    const image: { [Key: string]: string } = images;
+
+    return image[code];
+  }
 }
