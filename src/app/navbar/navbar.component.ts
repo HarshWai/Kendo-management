cc9e440bf7e608d3934c778061f0612ca3805ca9
HarshWai <harshvardhan.gunjal@waiin.com>
2025-04-22 15:24:40 +0530

    add dark mode

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -10,4 +10,34 @@ import { Component } from '@angular/core';
 export class NavbarComponent {
 
 
+  isDarkMode = false;
+  ngOnInit() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+      this.enableDarkMode();
+      this.isDarkMode = true;
+    }
+  }
+
+
+  toggleDarkMode(event: any) {
+    this.isDarkMode = event.target.checked;
+    if (this.isDarkMode) {
+      this.enableDarkMode();
+      localStorage.setItem('theme', 'dark');
+    } else {
+      this.disableDarkMode();
+      localStorage.setItem('theme', 'light');
+    }
+  }
+
+  enableDarkMode() {
+    document.body.classList.add('dark-mode');
+  }
+
+  disableDarkMode() {
+    document.body.classList.remove('dark-mode');
+  }
+
+
 }
