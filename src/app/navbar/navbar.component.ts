0ba72c093eae61bbdc1ab5a101474290404b6e3b
HarshWai <harshvardhan.gunjal@waiin.com>
2025-04-24 14:52:32 +0530

    dark mode button change

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
